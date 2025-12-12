69b1105c7bf266d930004baa4ace8d4a1959cb56
Davi Martins <daviairesg6@gmail.com>
2025-12-12 00:23:12 -0300

    api update

--- lib/api.ts
+++ lib/api.ts
@@ -1,1 +1,18 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+export const API_URL = "";
+
+export const fetcher = async (url: string, options: RequestInit = {}) => {
+  const res = await fetch(`${API_URL}${url}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    ...options,
+  });
+
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(errorText || "Erro na requisição");
+  }
+
+  return res.json();
+};
